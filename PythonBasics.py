475f05c133ffb66dc287e079ee83ba3053537384
Ehtisham Butt <buttehtisham123@gmail.com>
2022-07-20 16:43:01 +0500

    How to print Data in Python and How to use Sep and End Arguments

--- PythonBasics.py
+++ PythonBasics.py
@@ -1,1 +1,11 @@
 print("Hello Python")
+
+a=15
+b=9
+print("The value of a is a=",a, "The value of bis b=",b,"=c")
+
+d=10
+print("Value of d=", d," Jack","Smith",sep=" Hello ",end=" Python ")
+
+print("A","B","C", sep='DEF')
+print("09", "12", "2016", sep="-", end='\n')
