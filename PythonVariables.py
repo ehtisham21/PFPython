669a9ace9258adb3127570eb05de37485c761eb0
Ehtisham Butt <buttehtisham123@gmail.com>
2022-07-26 13:05:58 +0500

    Python Variables

--- PythonVariables.py
+++ PythonVariables.py
@@ -3,3 +3,30 @@ Class="BSCS"
 Subject="Python"
 
 print("Name:",name,"Class:",Class,"Subject:",Subject)
+
+print(id(name))
+print(id(Class))
+print(id(Subject))
+
+#Multiple Assignment
+
+x=y=z="Alex"
+print(x,y,z)
+
+a,b,c="Tom","John","Smart"
+print(a,b,c)
+
+#Deleting a Vairable
+"""del a,b,c
+print(a,b,c)"""
+
+
+#Unpacking a list into a variable
+
+listp=["Apple","Juice","Orange"]
+f=listp
+print(f)
+
+listq=["Name","GPA","CGPA"]
+u,i,h=listq
+print(u,i,h)
