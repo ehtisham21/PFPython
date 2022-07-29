d529de7b7542d8f4d5f0a89ee89aeafd6b4d7304
Ehtisham Butt <buttehtisham123@gmail.com>
2022-07-29 15:06:12 +0500

    Python Keywords

--- PythonKeywords.py
+++ PythonKeywords.py
@@ -57,3 +57,37 @@ print(True is False)
 print(True is True)
 print(True is not False)
 print(2+2 is 3)
+
+#Python Exception
+print("Python Exception")
+t=4
+y=0
+try:
+    q=t/y
+    print(q)
+
+except Exception as e:
+    print(e)
+
+finally:
+    print("This block will run every time must if exception is raised or not")
+
+assert y ==1, "Number in the y variable is 0"   #It will only given Assertion error if the statement is not True
+
+#Pass Keyword (Empty code is not allowed in loops, function definitions, class definitions, or in if statements so we can just add pass keyword in body to use that empty function for future.
+p=4
+q=3
+if a>b:
+    pass
+
+#Del Keyword
+op=2
+oo=3
+print(op,oo)
+del op
+print(op)
+
+listoo=["Smith","Alex",]
+print(listoo)
+del listoo[0]
+print(listoo)
