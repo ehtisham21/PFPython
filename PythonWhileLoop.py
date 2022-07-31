d465b31651fb8610e7bc5ce7cb6120dad1160fb5
Ehtisham Butt <buttehtisham123@gmail.com>
2022-07-31 17:02:47 +0500

    PythonWhileLoop

---
+++ PythonWhileLoop.py
@@ -0,0 +1,21 @@
+a=0;
+while a<=10:
+    print("While", a)
+    a += 1
+else:
+    print("Loop Ended")
+
+
+b=10
+while b>=0:
+    print("While",b)
+    b-=1
+else:
+    print("Loop Ended")
+jeef= 0
+count=0
+while jeef<=5:
+    print("Loop Counted")
+    jeef+=1
+    count+=1
+print("Your Loop Counted",count)
